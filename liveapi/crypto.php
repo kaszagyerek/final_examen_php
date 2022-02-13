a8034ad0ad7f6d9b4661fdae2e899f563a47b542
kaszagyerek <kasza.sandor@yahoo.com>
2022-02-13 15:24:35 +0200

    pofonosztas

--- liveapi/crypto.php
+++ liveapi/crypto.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../log_reg/connection.php";
 
+
 if(isset($_POST['lekerdezi'])) {
     $url = 'https://api2.binance.com/api/v3/ticker/24hr';
     $ch = curl_init($url);
@@ -35,10 +36,56 @@ if (isset($_POST['torli'])){
 
 }
 
+/*
+define("INTERVAL", 50 ); // 1 óra
+
+function runIt() { // Your function to run every 5 seconds
+    $url = 'https://api2.binance.com/api/v3/ticker/24hr';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_json = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response_json, true);
+    $products = $response;
+
+    foreach ($products as $product) {
+        $sym = $product["symbol"];
+        $price = $product["lastPrice"];
+        $sql = "UPDATE `crypto` SET `cryptosymbol`='$sym',`lastprice`='$price'";
+
+        if ($con->query($sql) === TRUE) {
+        } else {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
+
+    }
+
+}
+function frisites()
+{
+    $active = true;
+    $nextTime = microtime(true) + INTERVAL; // Set initial delay
+
+    while ($active) {
+        usleep(1000); // optional, if you want to be considerate
+
+        if (microtime(true) >= $nextTime) {
+            runIt();
+            $nextTime = microtime(true) + INTERVAL;
+        }
+    }
+}
+
+frisites();
+*/
 ?>
+
+
 
 <form method="post">
     <input type="submit" name="lekerdezi" class="button" value="lekerdezi" />
     <input type="submit" name="torli" class="button" value="torli" />
 
 </form>
+
---
+++ liveapi/frissites.php
@@ -0,0 +1,60 @@
+<?php
+
+define("INTERVAL", 50 ); // 1 óra
+
+function runIt() { // Your function to run every 5 seconds
+    $url = 'https://api2.binance.com/api/v3/ticker/24hr';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_json = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response_json, true);
+    $products = $response;
+    $con = mysqli_connect("localhost","root","","social");
+    if (mysqli_connect_errno())
+    {
+        echo "Hibás csatlakozás" . mysqli_connect_errno();
+    }
+
+    $sql1 = "DELETE FROM crypto";
+
+    if ($con->query($sql1) === TRUE) {
+    } else {
+        echo "Error: " . $sql . "<br>" . $con->error;
+    }
+    $sql1 = "ALTER TABLE  crypto AUTO_INCREMENT = 1";
+
+    if ($con->query($sql1) === TRUE) {
+    } else {
+        echo "Error: " . $sql . "<br>" . $con->error;
+    }
+    foreach ($products as $product) {
+        $sym = $product["symbol"];
+        $price = $product["lastPrice"];
+
+        $sql = "INSERT INTO `crypto` (cryptosymbol, lastprice) VALUES ( '$sym','$price');";
+
+        if ($con->query($sql) === TRUE) {
+        } else {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
+    }
+
+}
+function frisites()
+{
+    $active = true;
+    $nextTime = microtime(true) + INTERVAL; // Set initial delay
+
+    while ($active) {
+        usleep(1000); // optional, if you want to be considerate
+
+        if (microtime(true) >= $nextTime) {
+            runIt();
+            $nextTime = microtime(true) + INTERVAL;
+        }
+    }
+}
+
+frisites();
